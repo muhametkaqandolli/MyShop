fadf780029d7f932807d996eb5a126f6bab0b49f
muhamet kaqandolli <muhamet_kaqandolli@hotmail.com>
2020-05-12 19:22:15 +0200

    Product Grouping Added

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,83 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductCategoryRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<ProductCategory> categories;
+
+        public ProductCategoryRepository()
+        {
+            categories = cache["categories"] as List<ProductCategory>;
+            if (categories == null)
+            {
+                categories = new List<ProductCategory>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["categories"] = categories;
+        }
+
+        public void Insert(ProductCategory c)
+        {
+            categories.Add(c);
+        }
+
+        public void Update(ProductCategory category)
+        {
+            ProductCategory categoryToUpdate = categories.Find(p => p.Id == category.Id);
+
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate = category;
+            }
+            else
+            {
+                throw new Exception("Category not found!");
+            }
+
+        }
+
+        public ProductCategory Find(string Id)
+        {
+            ProductCategory category = categories.Find(p => p.Id == Id);
+
+            if (category != null)
+            {
+                return category;
+            }
+            else
+            {
+                throw new Exception("Category not found!");
+            }
+        }
+
+        public IQueryable<ProductCategory> Collection()
+        {
+            return categories.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            ProductCategory categoryToDelete = categories.Find(p => p.Id == Id);
+
+            if (categoryToDelete != null)
+            {
+                categories.Remove(categoryToDelete);
+            }
+            else
+            {
+                throw new Exception("Category not found!");
+            }
+        }
+    }
+}
--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Owin.Security.Notifications;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 using System;
 using System.Collections.Generic;
@@ -13,10 +14,12 @@ namespace MyShop.WebUI.Controllers
     public class ProductManagerController : Controller
     {
         ProductRepository context;
+        ProductCategoryRepository productCategories;
 
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository(); 
         }
         // GET: ProductManager
         public ActionResult Index()
@@ -27,8 +30,10 @@ namespace MyShop.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
         }
         [HttpPost]
         public ActionResult Create(Product product)
@@ -54,7 +59,10 @@ namespace MyShop.WebUI.Controllers
                 return HttpNotFound();
             } else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                viewModel.Product = product;
+                viewModel.ProductCategories = productCategories.Collection();
+                return View(viewModel);
             }
         }
 
